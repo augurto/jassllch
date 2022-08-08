878ce039c1ae565460d052a8ef3dba0f85a405c2
Ego <augurto.17@gmail.com>
2022-08-08 17:07:09 -0500

    plus

--- includes/usuarios_jass.php
+++ includes/usuarios_jass.php
@@ -62,8 +62,8 @@
                                         </button>
                          
                                          <button type="button" id="btnmodal" class="btn btn-dark" data-toggle="modal" data-target="#ModalEdit" data-nom="<?php echo $codigo; ?>" data-nom2="<?php echo $nombre_proyecto; ?>" data-ape="<?php echo $presupuesto;?>" data-estado="<?php echo $estado;  ?>" data-sub2="<?php echo $extencion;  ?>" >
-                                         <i class="fa fa-edit"></i>
-                                         <i class="fa fa-usd"></i>
+                                         <i class="fa fa-plus"></i>
+                                        
                                         </button>
                             </td>
                         </tr>
