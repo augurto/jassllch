60aae05cade7f32bf9fe033a8065dcbacaf7ca63
Ego <augurto.17@gmail.com>
2022-08-08 02:58:36 -0500

    subiendo pagos

--- index.php
+++ index.php
@@ -34,8 +34,7 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <link rel="stylesheet" href="css/style.css">
 
     <!-- inicio datatables -->
-    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
-    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/css/bootstrap-select.min.css'>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <!-- CSS personalizado --> 
@@ -100,42 +99,7 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <!-- Fin del contenido de la tabla -->
 
   
-    <div class="container">
-    <div class="row">
-      <h2>Bootstrap Select con buscador en vivo PHP MySQL</h2>
-      <p>Buscador select en vivo</p>
-      <hr />
-    
-    <div class="row-fluid">
-      <select class="selectpicker" data-show-subtext="true" data-live-search="true">
-
-<?php
-include "select_buscador/db.php";
-$con = connect();
-if (!$con->set_charset("utf8")) {//asignamos la codificación comprobando que no falle
-       die("Error cargando el conjunto de caracteres utf8");
-}
-$consulta = "SELECT * FROM usuarios_jass";
-$resultado = mysqli_query($con , $consulta);
-$contador=0;
-
-while($misdatos = mysqli_fetch_assoc($resultado)){ $contador++;?>
-<option data-subtext="<?php echo $misdatos["id_jass"]; ?>"><?php echo $misdatos["nombres"]; ?></option>
-<?php }?>          
-</select>
-
-    </div><hr />
-    </div> 
-     
-  </div>
-
-   <!-- Contenido -->    
-   
- <br><br>
-
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>  
+    <?php  include 'includes/footer.php'?>
 
 
     <!-- Inicio de Script para datatables -->
---
+++ pagos.php
@@ -0,0 +1,41 @@
+<html>
+  <head>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
+  </head>
+
+  <div class="container">
+    <div class="row">
+      <h2>Bootstrap-select example</h2>
+      <p>This uses <a href="https://silviomoreto.github.io/bootstrap-select/">https://silviomoreto.github.io/bootstrap-select/</a></p>
+      <hr />
+    </div>
+
+    <div class="row-fluid">
+      <select class="selectpicker" data-show-subtext="true" data-live-search="true">
+        <option data-subtext="Rep California">Tom Foolery</option>
+        <option data-subtext="Sen California">Bill Gordon</option>
+        <option data-subtext="Sen Massacusetts">Elizabeth Warren</option>
+        <option data-subtext="Rep Alabama">Mario Flores</option>
+        <option data-subtext="Rep Alaska">Don Young</option>
+        <option data-subtext="Rep California" disabled="disabled">Marvin Martinez</option>
+      </select>
+      <span class="help-inline">With <code>data-show-subtext="true" data-live-search="true"</code>. Try searching for california</span>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
