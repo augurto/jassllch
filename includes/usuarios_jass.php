79053b3d1646c11596702413c29e57ed9fa05d3f
Ego <augurto.17@gmail.com>
2022-08-08 17:03:10 -0500

    warning

--- includes/usuarios_jass.php
+++ includes/usuarios_jass.php
@@ -61,7 +61,7 @@
                                         <i class="fa fa-edit"></i>
                                         </button>
                          
-                                         <button type="button" id="btnmodal" class="btn btn-dark" data-toggle="modal" data-target="#ModalEdit" data-nom="<?php echo $codigo; ?>" data-nom2="<?php echo $nombre_proyecto; ?>" data-ape="<?php echo $presupuesto;?>" data-estado="<?php echo $estado;  ?>" data-sub2="<?php echo $extencion;  ?>" >
+                                         <button type="button" id="btnmodal" class="btn btn-warning" data-toggle="modal" data-target="#ModalEdit" data-nom="<?php echo $codigo; ?>" data-nom2="<?php echo $nombre_proyecto; ?>" data-ape="<?php echo $presupuesto;?>" data-estado="<?php echo $estado;  ?>" data-sub2="<?php echo $extencion;  ?>" >
                                         <i class="fa fa-money"></i>
                                         </button>
                             </td>
