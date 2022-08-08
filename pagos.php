a003db9183beb62d1f562fa7d7499bf750fbddda
Ego <augurto.17@gmail.com>
2022-08-08 03:12:03 -0500

    XD

--- pagos.php
+++ pagos.php
@@ -6,36 +6,32 @@
 
   <div class="container">
     <div class="row">
-      <h2>Bootstrap-select example</h2>
-      <p>This uses <a href="https://silviomoreto.github.io/bootstrap-select/">https://silviomoreto.github.io/bootstrap-select/</a></p>
+      
       <hr />
-    </div>
-
+    
     <div class="row-fluid">
       <select class="selectpicker" data-show-subtext="true" data-live-search="true">
-        <option data-subtext="Rep California">Tom Foolery</option>
-        <option data-subtext="Sen California">Bill Gordon</option>
-        <option data-subtext="Sen Massacusetts">Elizabeth Warren</option>
-        <option data-subtext="Rep Alabama">Mario Flores</option>
-        <option data-subtext="Rep Alaska">Don Young</option>
-        <option data-subtext="Rep California" disabled="disabled">Marvin Martinez</option>
-      </select>
-      <span class="help-inline">With <code>data-show-subtext="true" data-live-search="true"</code>. Try searching for california</span>
-    </div>
+
+<?php 
+include "select_buscador/db.php";
+$con = connect();
+if (!$con->set_charset("utf8")) {//asignamos la codificación comprobando que no falle
+       die("Error cargando el conjunto de caracteres utf8");
+}
+$consulta = "SELECT * FROM usuarios_jass";
+$resultado = mysqli_query($con , $consulta);
+$contador=0;
+
+while($misdatos = mysqli_fetch_assoc($resultado)){ $contador++;?>
+<option data-subtext="<?php echo $misdatos["id_jass"]; ?>"><?php echo $misdatos["nombres"]; ?></option>
+<?php }?>          
+</select>
+
+    </div><hr />
+    </div> 
+     
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
-</html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
 </html>
