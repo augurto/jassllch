6aca2f3d328b1f2d192b0d2d19cb94262e3aa6a1
Ego <augurto.17@gmail.com>
2022-08-09 08:39:24 -0500

    iamgen abajo y centrado

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -6,11 +6,12 @@ $pdf = new FPDF('L','mm','A5');
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 // Insert a logo in the top-left corner at 300 dpi
-$pdf->Image('../../img/logo1.jpeg',10,10,-300);
+
 // Insert a dynamic image from a URL
 
-$pdf->Cell(100,10,'Dni :');
+$pdf->Cell(100,10,'Dni :'.$valor1);
 $pdf->Cell(110,10,$valor1);
+$pdf->Image('../../img/logo1.jpeg', 80 ,22, 35 , 38);
 
 $pdf->Output();
 ?>
