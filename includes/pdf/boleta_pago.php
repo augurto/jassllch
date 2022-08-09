c7c7b4a671987cc56eead62ecdb87c6389ae7c70
Ego <augurto.17@gmail.com>
2022-08-09 08:00:50 -0500

    target blank

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -2,7 +2,7 @@
 require('../../fpdf/fpdf.php');
 
 
-$pdf = new FPDF('P','mm','A5');
+$pdf = new FPDF('L','mm','A5');
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
