5dc5052e02e1a8029037a78b9785e6e61e4c6ef1
Ego <augurto.17@gmail.com>
2022-08-09 08:56:50 -0500

    cambio de posicion

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -12,10 +12,10 @@ $pdf->SetFont('Arial','B',16);
 // Insert a logo in the top-left corner at 300 dpi
 
 // Insert a dynamic image from a URL
+$pdf->Cell(70,20,'Usuario : '.$datos);
+$pdf->Cell(80,10,'Dni : '.$valor1);
 
-$pdf->Cell(100,10,'Dni : '.$valor1);
-$pdf->Cell(100,20,'Usuario : '.$datos);
-$pdf->Cell(100,30,'Fecha de pago : '.$fecha_pago);
+$pdf->Cell(90,30,'Fecha de pago : '.$fecha_pago);
 $pdf->Cell(100,40,'Atendido por : '.$usuario_atencion);
 $pdf->Cell(105,50,'Mes cancelado : '.$pago_mes);
 
