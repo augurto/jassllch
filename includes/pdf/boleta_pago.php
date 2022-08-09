04c32e9813f6e6366da397058ab194a95f2885a3
Ego <augurto.17@gmail.com>
2022-08-09 08:14:43 -0500

    subiendo logo

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -5,6 +5,10 @@ require('../../fpdf/fpdf.php');
 $pdf = new FPDF('L','mm','A5');
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
+// Insert a logo in the top-left corner at 300 dpi
+$pdf->Image('../../img/logo1.jpeg',10,10,-300);
+// Insert a dynamic image from a URL
+
 $pdf->Cell(40,10,'Hello World!');
 $pdf->Output();
 ?>
