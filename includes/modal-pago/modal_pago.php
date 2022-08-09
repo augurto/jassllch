9dc936df6cd5e5d11ea67451527e888c25b36101
Ego <augurto.17@gmail.com>
2022-08-09 03:58:43 -0500

    suma en tiempo real

--- includes/modal-pago/modal_pago.php
+++ includes/modal-pago/modal_pago.php
@@ -35,6 +35,30 @@
                     echo $var_PHP   // muestra el resultado 
 
                     ?></label>
+                    <script>
+                        //Función que realiza la suma
+                        function Suma() {
+                        var ingreso1 = document.calculadora.ingreso1.value;
+                        var ingreso2 = document.calculadora.ingreso2.value;
+                        try{
+                            //Calculamos el número escrito:
+                            ingreso1 = (isNaN(parseInt(ingreso1)))? 0 : parseInt(ingreso1);
+                            ingreso2 = (isNaN(parseInt(ingreso2)))? 0 : parseInt(ingreso2);
+                            document.calculadora.resultado.value = ingreso1+ingreso2;
+                        }
+                        //Si se produce un error no hacemos nada
+                        catch(e) {}
+                        }
+                        </script>
+
+                        El evento onKeyUp se realiza cuando sueltas la tecla. Entonces es cuando se llama a la función Suma
+                        <form name="calculadora">
+                        Ingrese números:<br>
+                        <input type="text" name="ingreso1" onKeyUp="Suma()"><br><br>
+                        <input type="text" name="ingreso2" onKeyUp="Suma()"><br><br>
+                        El resultado es:<br>
+                        <input type="text" name="resultado" disabled><br>
+                        </form>
                     <form action="../../includes/process/actualizar/actualizar_proyecto.php">
                         <div class="modal-body">
                         <div class="input-group mb-3">
