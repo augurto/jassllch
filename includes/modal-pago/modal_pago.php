eb6d060f68081a78de3f6217568ff402eb62905f
Ego <augurto.17@gmail.com>
2022-08-09 03:26:32 -0500

    mejorando la idea del select

--- includes/modal-pago/modal_pago.php
+++ includes/modal-pago/modal_pago.php
@@ -48,14 +48,17 @@
                         </div>
                         <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1">Mes</span>
-                        <input type="text" class="form-control" id="mes"  aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo date("m");?>" >
-                        <select>
-                        <option value="January"<?php if (date("m") == 07) echo ' selected="selected"'; ?>>Julio</option>
-                        <option value="January"<?php if (date("m") == 08) echo ' selected="selected"'; ?>>Agosto</option>
-                        <option value="January"<?php if (date("m") == 09) echo ' selected="selected"'; ?>>Setiembre</option>
-                        <option value="January"<?php if (date("m") == 10) echo ' selected="selected"'; ?>>Octubre</option>
+                        <?php 
+                        $mes_actual =strtotime(date("m"));
+                        ?>
+                        <input type="text" class="form-control" id="mes"  aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $mes_actual;?>" >
+                        <!-- <select>
+                        <option value="January"<?php if (date("m") = 07) echo ' selected="selected"'; ?>>Julio</option>
+                        <option value="January"<?php if (date("m") = 08) echo ' selected="selected"'; ?>>Agosto</option>
+                        <option value="January"<?php if (date("m") = 09) echo ' selected="selected"'; ?>>Setiembre</option>
+                        <option value="January"<?php if (date("m") = 10) echo ' selected="selected"'; ?>>Octubre</option>
                            
-                        </select>
+                        </select> -->
                         </div>
                         <div class="input-group mb-3">
                                 <label class="input-group-text" for="inputGroupSelect01">Sub Tipo Proyecto</label>
@@ -87,7 +90,7 @@
                             <select class="form-select" aria-label="Default select example" name="estado" id="estado">
                             <option selected>Seleciona Estado</option>
                             <option value="0">Pendiente</option>
-                            <option value="1">Terminado</option>
+                            <option value="1" selected>Terminado</option>
                             <option value="2">Inactivo</option>
                             </select>
                         </div>
