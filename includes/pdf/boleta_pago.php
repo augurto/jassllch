f4c2b7a44fd00cfe74573bb2b6f28db1fc43d48c
Ego <augurto.17@gmail.com>
2022-08-09 09:18:01 -0500

    guia con lo de puilar

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -13,8 +13,10 @@ $pdf->SetFont('Arial','B',16);
 
 // Insert a dynamic image from a URL
 $pdf->Ln(5);
+$pdf->SetFont('Arial','B',12);
 $pdf->Cell(0,10,'Usuario : ');
-$pdf->Cell(1,10,$datos);
+$pdf->SetFont('Arial','',12);
+$pdf->Cell(0,10,$datos);
 $pdf->Ln();
 $pdf->Cell(0,10,'Dni : '.$valor1);
 $pdf->Ln();
