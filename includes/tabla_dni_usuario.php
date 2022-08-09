30b6031a9f5d0e3ea5ed5495c685a1512ef39d4e
Ego <augurto.17@gmail.com>
2022-08-09 07:57:39 -0500

    as

--- includes/tabla_dni_usuario.php
+++ includes/tabla_dni_usuario.php
@@ -65,7 +65,7 @@
                                          <!-- <button type="button" id="btnmodal0" class="btn btn-primary" data-toggle="modal" data-target="#ModalEdit0" data-id_jass="<?php echo $codigo; ?>" data-nombre="<?php echo $nombre; ?>" data-ape="<?php echo $presupuesto;?>" data-estado="<?php echo $estado;  ?>" data-sub2="<?php echo $extencion;  ?>" >
                                         <i class="fa fa-edit"></i>
                                         </button> -->
-                                        <a href="./pdf/boleta_pago.php">
+                                        <a href="../includes/pdf/boleta_pago.php">
                                          <button type="button" id="btnmodal" class="btn btn-dark" data-toggle="modal" data-target="#ModalEdit" data-jass="<?php echo $id_jass; ?>" data-nombre="<?php echo $nombre; ?>" data-paterno="<?php echo $ap_paterno;?>" data-materno="<?php echo $ap_materno;  ?>" data-dni="<?php echo $dni;  ?>" >
                                          <i class="fa fa-print"></i>
                                          
