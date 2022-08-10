78948d3735459b3ed26084cb0518d6a7463ccf36
Ego <augurto.17@gmail.com>
2022-08-09 19:49:39 -0500

    acomodando fondo

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -21,7 +21,7 @@ $pdf->setX(10);
 // Agregamos los datos de la empresa
 $pdf->Cell(5,$textypos,"Jass");
 $pdf->Image("../../img/logo2.jpeg", 10,10,20,20,'JPEG' );
-$pdf->Image("../../img/fondo3.png", 0,10,150,150,'PNG' );
+$pdf->Image("../../img/fondo3.png", 0,0,150,180,'PNG' );
 /* $pdf->SetFont('Arial','B',10);    
 $pdf->setY(10);$pdf->setX(10);
 $pdf->Cell(5,$textypos,"DE:");
