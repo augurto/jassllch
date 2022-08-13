9bc8bb5a4485993e636a782647155a516bcabcef
Ego <augurto.17@gmail.com>
2022-08-13 12:28:17 -0500

    registrar egreso

--- includes/graficas/graficas_inicio.php
+++ includes/graficas/graficas_inicio.php
@@ -43,12 +43,13 @@
             
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
+              <button type="button" class="btn btn-primary" id="boton_proyecto" data-toggle="modal" data-target="#exampleModalCenter">
               <!-- <a href="../../index.php"> -->
-              <a href="#">
+        
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Usuarios</div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Registrar Egreso</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tps;?></div>
                     </div>
                     <div class="col-auto">
@@ -56,7 +57,8 @@
                     </div>
                   </div>
                 </div>
-                </a>
+            
+                </button>
               </div>
             </div>
             
@@ -90,7 +92,7 @@
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Registrar Pago</div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Registrar Ingreso</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tin;?></div>
                     </div>
                     <div class="col-auto">
