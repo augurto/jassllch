907c7be72c0b1abcaacdd906534c364b90a922fc
Ego <augurto.17@gmail.com>
2022-08-15 04:15:50 -0500

    qw12

--- reporte_general.php
+++ reporte_general.php
@@ -81,21 +81,41 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
   
     <div class="container">
     <div class="row">
-      
-    <div class="grid text-center">
-      <div class="g-col-6">
-        <form action="#4">
-      <div class="input-group">
-                      <input type="text" class="form-control" aria-label="Text input with segmented dropdown button" placeholder="Digite el año">
-                      <button class="btn btn-outline-secondary" type="submit">Reporte Año</button>
-      </div>
-      </form>
+    <form class="row row-cols-lg-auto g-3 align-items-center">
+  <div class="col-12">
+    <label class="visually-hidden" for="inlineFormInputGroupUsername">Username</label>
+    <div class="input-group">
+      <div class="input-group-text">@</div>
+      <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username">
     </div>
-      <div class="g-col-6">.g-col-6</div>
+  </div>
+
+  <div class="col-12">
+    <label class="visually-hidden" for="inlineFormSelectPref">Preference</label>
+    <select class="form-select" id="inlineFormSelectPref">
+      <option selected>Choose...</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+  </div>
+
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="inlineFormCheck">
+      <label class="form-check-label" for="inlineFormCheck">
+        Remember me
+      </label>
     </div>
+  </div>
+
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</form>
 
 
-    <form action="#1">
+
     <div class="input-group">
       <form action="#1">
                                   <select name="mes_pago" id="mes_pago" class="form-select" required>
@@ -122,12 +142,12 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
                                           } 
                                       ?> 
                                 </select>
-                                <button class="btn btn-outline-secondary" type="submit">Reporte Mensual</button>
+                                <button class="btn btn-outline-secondary" type="button">Reporte Mensual</button>
       </form>
                        
-      <form action="#4">
+      <form action="">
                       <input type="text" class="form-control" aria-label="Text input with segmented dropdown button" placeholder="Digite el año">
-                      <button class="btn btn-outline-secondary" type="submit">Reporte Año</button>
+                      <button class="btn btn-outline-secondary" type="button">Reporte Año</button>
       </form>
         </div>
         
