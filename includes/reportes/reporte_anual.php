478184c1c0105275e4dd69554bafc3deb94da18a
Ego <augurto.17@gmail.com>
2022-08-15 06:24:12 -0500

    id_pagos

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -1,7 +1,7 @@
 <?php
 require_once("./mysql_table.php");
 $db_handle = new DBController();
-$result = $db_handle->runQuery("SELECT datos_pago FROM pagos");
+$result = $db_handle->runQuery("SELECT id_pagos FROM pagos");
 $header = $db_handle->runQuery("SELECT datos_pago 
 FROM `INFORMATION_SCHEMA`.`COLUMNS` 
 WHERE `TABLE_SCHEMA`='blog_samples' 
