409cf9475b8422879715a927f7ca03157a700144
Ego <augurto.17@gmail.com>
2022-08-15 01:05:06 -0500

    mes para reporte

--- includes/tabla_reporte_general.php
+++ includes/tabla_reporte_general.php
@@ -32,7 +32,7 @@
                         $nombre=$row['datos_pago'];
                         $pago_hoy=$row['pago_hoy'];
                         $name_user=$row['name_user'];
-                        $pago_mes=$row['pago_mes'];
+                        $pago_mes=$row['mes_actual_pago'];
                         $monto_pago=$row['monto_pago'];
                         $deuda=$row['deuda'];
                         $vuelto=$row['vuelto'];
@@ -66,7 +66,51 @@
                             
                             <td><?php echo $pago_hoy ?></td>
                             <td><?php echo $name_user ?></td>
-                            <td><?php echo $pago_mes ?></td>
+                            <td><?php 
+                            
+                            switch ($pago_mes) {
+                                case 1:
+                                    echo "Enero";
+                                    break;
+                                    case 2:
+                                        echo "Febrero";
+                                        break;
+                                        case 3:
+                                            echo "Marzo";
+                                            break;
+                                            case 4:
+                                                echo "Abril";
+                                                break;
+                                                case 5:
+                                                    echo "Mayo";
+                                                    break;
+                                                    case 6:
+                                                        echo "Junio";
+                                                        break;
+                                                        case 7:
+                                                            echo "Julio";
+                                                            break;
+                                                            case 8:
+                                                                echo "Agosto";
+                                                                break;
+                                                                case 9:
+                                                                    echo "Septiembre";
+                                                                    break;
+                                                                    case 10:
+                                                                        echo "Octubre";
+                                                                        break;
+                                                                        case 11:
+                                                                            echo "Noviembre";
+                                                                            break;
+                                                                            case 12:
+                                                                                echo "Diciembre";
+                                                                                break;
+                                
+                            }
+                            ?>
+                            
+                            
+                            ?></td>
                             <td><?php echo $deuda ?></td>
                             <td><?php if ($estado==0) {
                                 # code...
