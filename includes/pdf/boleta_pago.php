c9b7aab506c42404fcefe9c111c180432c70d7ba
Ego <augurto.17@gmail.com>
2022-08-15 01:56:54 -0500

    de nuevo

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -73,21 +73,15 @@ $pdf->setY(40);$pdf->setX(135);
 //// Array de Cabecera
 $header = array("Nro", "Concepto","Cant.","Precio","Total");
 //// Arrar de Productos
-if ($estado=0) {
-    # code...
-    $products = array(
+$products = array(
     
-        array("01", "Agua",1,$monto_usuario,0),
-        
-    );
-}else{
-    $products = array(
-    
-        array("01", "Pago",1,$monto_usuario,0),
-        
-    );
-}
-
+	array("01", $estado,1,$monto_usuario,0),
+	/* array("0024", "Producto 2",5,80,0),
+	array("0001", "Producto 3",1,40,0),
+	array("0001", "Producto 3",5,80,0), 
+    array("0001", "Producto 3",4,30,0),
+	array("0001", "Producto 3",7,80,0), */
+);
     // Column widths
     $w = array(20, 95, 20, 25, 25);
     // Header
