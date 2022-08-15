52b92ba6ce8547c57a51d4dfb559aab830e5b07c
Ego <augurto.17@gmail.com>
2022-08-15 05:49:50 -0500

    zxca

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -22,5 +22,11 @@ foreach($result as $row) {
 	foreach($row as $column)
 		$pdf->Cell(30,12,$column,1);
 }
+foreach($result as $row) {
+	$pdf->SetFont('Arial','',12);	
+	$pdf->Ln();
+	foreach($row as $column)
+		$pdf->Cell(30,12,$column,1);
+}
 $pdf->Output();
 ?>
