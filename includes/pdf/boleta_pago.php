c352a4fd58ed0270ae883fb730644d93ddc03755
Ego <augurto.17@gmail.com>
2022-08-15 01:51:17 -0500

    xcv

--- includes/pdf/boleta_pago.php
+++ includes/pdf/boleta_pago.php
@@ -75,19 +75,19 @@ $header = array("Nro", "Concepto","Cant.","Precio","Total");
 //// Arrar de Productos
 if ($estado=0) {
     # code...
-    $estado_nuevo='Agua';
-}else{
-    $estado_nuevo='Pago';
-}
-$products = array(
+    $products = array(
     
-	array("01", $estado_nuevo,1,$monto_usuario,0),
-	/* array("0024", "Producto 2",5,80,0),
-	array("0001", "Producto 3",1,40,0),
-	array("0001", "Producto 3",5,80,0), 
-    array("0001", "Producto 3",4,30,0),
-	array("0001", "Producto 3",7,80,0), */
-);
+        array("01", "Agua",1,$monto_usuario,0),
+        
+    );
+}else{
+    $products = array(
+    
+        array("01", "Pago",1,$monto_usuario,0),
+        
+    );
+}
+
     // Column widths
     $w = array(20, 95, 20, 25, 25);
     // Header
