b3acce24b96d295d30bf360c860642bba7e8f6cf
Ego <augurto.17@gmail.com>
2022-08-15 05:06:58 -0500

    zxc

---
+++ includes/reportes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('DB_HOST','localhost');
+    define('DB_NAME','u415020159_jass');
+    define('DB_USER','u415020159_jass');
+    define('DB_PASSWORD','JassJass*#17');
+    define('DB_CHARSET','utf8');
+
+?>
