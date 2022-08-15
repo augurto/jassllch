81d11aea39813baee1d34a4e28518834e7c39219
Ego <augurto.17@gmail.com>
2022-08-15 05:58:08 -0500

    Multicell

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -14,13 +14,13 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','B',12);		
 foreach($header as $heading) {
 	foreach($heading as $column_heading)
-		$pdf->CellFitSpace(30,12,$column_heading,1);
+		$pdf->Multicell(30,12,$column_heading,1);
 }
 foreach($result as $row) {
 	$pdf->SetFont('Arial','',12);	
 	$pdf->Ln();
 	foreach($row as $column)
-		$pdf->CellFitSpace(30,12,$column,1);
+		$pdf->Multicell(30,12,$column,1);
 }
 
 $pdf->Output();
