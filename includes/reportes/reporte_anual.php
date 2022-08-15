690ea558cebe03ac300679b3ec5da82bad04b02f
Ego <augurto.17@gmail.com>
2022-08-15 05:56:07 -0500

    CellFitSpace

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -14,19 +14,14 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','B',12);		
 foreach($header as $heading) {
 	foreach($heading as $column_heading)
-		$pdf->Cell(30,12,$column_heading,1);
+		$pdf->CellFitSpace(30,12,$column_heading,1);
 }
 foreach($result as $row) {
 	$pdf->SetFont('Arial','',12);	
 	$pdf->Ln();
 	foreach($row as $column)
-		$pdf->Cell(30,12,$column,1);
+		$pdf->CellFitSpace(30,12,$column,1);
 }
-foreach($result as $row) {
-	$pdf->SetFont('Arial','',12);	
-	
-	foreach($row as $column)
-		$pdf->Cell(30,12,$column,1);
-}
+
 $pdf->Output();
 ?>
