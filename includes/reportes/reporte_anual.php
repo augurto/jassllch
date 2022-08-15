aad018a58d589dd8c573be44ea6af102348753c2
Ego <augurto.17@gmail.com>
2022-08-15 05:38:16 -0500

    otra tabla php

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -1,35 +1,25 @@
 <?php
-require('mysql_table.php');
+require_once("./mysql_table.php");
+$db_handle = new DBController();
+$result = $db_handle->runQuery("SELECT * FROM pagos");
+$header = $db_handle->runQuery("SELECT `COLUMN_NAME` 
+FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+WHERE `TABLE_SCHEMA`='blog_samples' 
+    AND `TABLE_NAME`='pagos'");
 
-class PDF extends PDF_MySQL_Table
-{
-function Header()
-{
-    // Title
-    $this->SetFont('Arial','',18);
-    $this->Cell(0,6,'World populations',0,1,'C');
-    $this->Ln(10);
-    // Ensure table header is printed
-    parent::Header();
-}
-}
-
-// Connect to database
-$link = mysqli_connect('localhost','u415020159_jass','JassJass*#17','u415020159_jass');
-
-$pdf = new PDF();
+require('../../fpdf/fpdf.php');
+$pdf = new FPDF();
 $pdf->AddPage();
-// First table: output all columns
-$pdf->Table($link,'select * from pagos order by id_pagos');
-$pdf->AddPage();
-// Second table: specify 3 columns
-$pdf->AddCol('rank',20,'','C');
-$pdf->AddCol('name',40,'datos_pagos');
-$pdf->AddCol('pop',40,'Pop (2001)','R');
-$prop = array('HeaderColor'=>array(255,150,100),
-            'color1'=>array(210,245,255),
-            'color2'=>array(255,255,210),
-            'padding'=>2);
-$pdf->Table($link,'select datos_pago, format(year_actual_pago,0) as pop, estado_pago from pagos order by id_pagos limit 0,10',$prop);
+$pdf->SetFont('Arial','B',12);		
+foreach($header as $heading) {
+	foreach($heading as $column_heading)
+		$pdf->Cell(90,12,$column_heading,1);
+}
+foreach($result as $row) {
+	$pdf->SetFont('Arial','',12);	
+	$pdf->Ln();
+	foreach($row as $column)
+		$pdf->Cell(90,12,$column,1);
+}
 $pdf->Output();
 ?>
