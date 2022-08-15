cabd2ff1246b5a6516d03adccd6d0c5adf1ccd5b
Ego <augurto.17@gmail.com>
2022-08-15 05:17:56 -0500

    ojalaaa

--- includes/reportes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('DB_HOST','localhost');
-    define('DB_NAME','u415020159_jass');
-    define('DB_USER','u415020159_jass');
-    define('DB_PASSWORD','JassJass*#17');
-    define('DB_CHARSET','utf8');
-
-?>
