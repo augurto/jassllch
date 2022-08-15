fbacdf53573e1d77c2b3860b0e1e09f36b441531
Ego <augurto.17@gmail.com>
2022-08-15 04:35:57 -0500

    faltaba el echo

--- reporte_general.php
+++ reporte_general.php
@@ -132,7 +132,7 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
     <form action="#2.1">
     <div class="input-group">
-                      <input type="text" class="form-control" aria-label="Text input with segmented dropdown button" value="<?php date("Y") ;?>" >
+                      <input type="text" class="form-control" aria-label="Text input with segmented dropdown button" value="<?php echo date("Y") ;?>" >
                       <button class="btn btn-outline-secondary" type="submit">Reporte Año</button>
     </div>
       </form>
