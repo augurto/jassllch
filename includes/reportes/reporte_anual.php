7e792f4761a492b7db12cabef471831c3ac6022c
Ego <augurto.17@gmail.com>
2022-08-15 06:49:34 -0500

    qwwq

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -2,6 +2,7 @@
 require_once("./mysql_table.php");
 $db_handle = new DBController();
 $result = $db_handle->runQuery("SELECT id_pagos, deuda, estado_pago FROM pagos");
+$result1 = $db_handle->runQuery("SELECT datos_pago FROM pagos");
 $header = $db_handle->runQuery("SELECT id_pagos , deuda, estado_pago
 FROM `INFORMATION_SCHEMA`.`COLUMNS` 
 WHERE `TABLE_SCHEMA`='blog_samples' 
@@ -15,6 +16,12 @@ $pdf->SetFont('Arial','B',12);
 foreach($header as $heading) {
 	foreach($heading as $column_heading)
 		$pdf->Cell(30,12,$column_heading,1);
+}
+foreach($result1 as $row1) {
+	$pdf->SetFont('Arial','',12);	
+	
+	foreach($row1 as $column1)
+		$pdf->Cell(30,12,$column1,1);
 }
 foreach($result as $row) {
 	$pdf->SetFont('Arial','',12);	
