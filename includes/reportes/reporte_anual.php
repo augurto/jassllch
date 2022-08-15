534710db9b4bbaf64e9578ec45a895b701c6caf3
Ego <augurto.17@gmail.com>
2022-08-15 05:45:32 -0500

    10 as

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -14,13 +14,13 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','B',12);		
 foreach($header as $heading) {
 	foreach($heading as $column_heading)
-		$pdf->Cell(90,12,$column_heading,1);
+		$pdf->Cell(10,12,$column_heading,1);
 }
 foreach($result as $row) {
 	$pdf->SetFont('Arial','',12);	
 	$pdf->Ln();
 	foreach($row as $column)
-		$pdf->Cell(90,12,$column,1);
+		$pdf->Cell(10,12,$column,1);
 }
 $pdf->Output();
 ?>
