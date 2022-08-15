a7763919be954b11ede5a13ab7b25dce009f1f97
Ego <augurto.17@gmail.com>
2022-08-15 05:44:02 -0500

    datos pago

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -1,8 +1,11 @@
 <?php
 require_once("./mysql_table.php");
 $db_handle = new DBController();
-$result = $db_handle->runQuery("SELECT id_pagos, dni_usuarios FROM pagos");
-$header = $db_handle->runQuery("SELECT id_pagos, dni_usuarios FROM pagos");
+$result = $db_handle->runQuery("SELECT * FROM pagos");
+$header = $db_handle->runQuery("SELECT datos_pago 
+FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+WHERE `TABLE_SCHEMA`='blog_samples' 
+    AND `TABLE_NAME`='pagos'");
 
 require('../../fpdf/fpdf.php');
 $pdf = new FPDF();
