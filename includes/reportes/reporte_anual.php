28e05c16418566f2d993e286afcb458d074e6deb
Ego <augurto.17@gmail.com>
2022-08-15 06:28:39 -0500

    czxc

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -1,8 +1,8 @@
 <?php
 require_once("./mysql_table.php");
 $db_handle = new DBController();
-$result = $db_handle->runQuery("SELECT id_pagos, dni_usuarios_jass, deuda,estado_pago FROM pagos");
-$header = $db_handle->runQuery("SELECT id_pagos, dni_usuarios_jass, deuda,estado_pago 
+$result = $db_handle->runQuery("SELECT datos_pago FROM pagos");
+$header = $db_handle->runQuery("SELECT datos_pago 
 FROM `INFORMATION_SCHEMA`.`COLUMNS` 
 WHERE `TABLE_SCHEMA`='blog_samples' 
     AND `TABLE_NAME`='pagos'");
@@ -20,8 +20,7 @@ foreach($result as $row) {
 	$pdf->SetFont('Arial','',12);	
 	$pdf->Ln();
 	foreach($row as $column)
-    $pdf->Cell(30,12,$column,1);
-    
+		$pdf->Cell(30,12,$column,1);
 }
 
 $pdf->Output();
