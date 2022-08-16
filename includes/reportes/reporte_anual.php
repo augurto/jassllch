2345f1f7d5831ab83b29439802322005bac8c1c1
Ego <augurto.17@gmail.com>
2022-08-16 09:15:53 -0500

    cell

--- includes/reportes/reporte_anual.php
+++ includes/reportes/reporte_anual.php
@@ -22,7 +22,7 @@ foreach($result as $row) {
 	$pdf->SetFont('Arial','',12);	
 	$pdf->Ln();
 	foreach($row as $column)
-		$pdf->Multicell(30,12,$column,1);
+		$pdf->Cell(30,12,$column,1);
 }
 
 $pdf->Output();
