693d174c600c7662b876f574be344d81c03d8154
Ego <augurto.17@gmail.com>
2022-08-16 10:10:37 -0500

    mysqli

--- includes/reportes/reporte_mensual.php
+++ includes/reportes/reporte_mensual.php
@@ -1,138 +1,75 @@
-<?php
+
+
+
+<?php error_reporting(0);
+
 require('../../fpdf/fpdf.php');
 
-$id_pago=$_GET['id_pago'];
-$valor1=$_GET['dni'];
-$datos=$_GET['datos'];
-$fecha_pago=$_GET['fecha_pago'];
-$usuario_atencion=$_GET['usuario_atencion'];
-$pago_mes=$_GET['pago_mes'];
-$monto_mes=$_GET['monto_mes'];
-$monto_usuario=$_GET['monto_usuario'];
-$vuelto=$_GET['vuelto'];
-$estado=$_GET['estado'];
+//Conecto a la base de datos
+$host_name = "localhost";
+$database = "u415020159_jass"; // Change your database nae
+$username = "u415020159_jass";          // Your database user id 
+$password = "JassJass*#17";    
+$connection=mysqli_connect($host_name,$username,$password,$database);
+$mysqli = new mysqli("localhost", "u415020159_jass", "JassJass*#17", "u415020159_jass");
+//Consulta la tabla productos solicitando todos los productos
+$resultado = $mysqli->query("SELECT Name FROM City LIMIT 10");
 
 
-$pdf = new FPDF('L','mm','A5');
+
+//Instaciamos la clase para genrear el documento pdf
+$pdf=new FPDF();
+
+//Agregamos la primera pagina al documento pdf
 $pdf->AddPage();
-$pdf->Image("../../img/fondo2.png", 15,20,180,110,'PNG' );
-$pdf->SetFont('Arial','B',16);    
-$textypos = 5;
-$pdf->setY(2);
-$pdf->setX(10);
-// Agregamos los datos de la empresa
-/* $pdf->Cell(5,$textypos,"Jass"); */
-$pdf->Image("../../img/logo2.jpeg", 10,10,30,30,'JPEG' );
 
-/* $pdf->SetFont('Arial','B',10);    
-$pdf->setY(10);$pdf->setX(10);
-$pdf->Cell(5,$textypos,"DE:");
-$pdf->SetFont('Arial','',10);    
-$pdf->setY(15);$pdf->setX(10);
-$pdf->Cell(5,$textypos,"Nombre de la empresa");
-$pdf->setY(20);$pdf->setX(10);
-$pdf->Cell(5,$textypos,"Direccion de la empresa");
-$pdf->setY(25);$pdf->setX(10);
-$pdf->Cell(5,$textypos,"Telefono de la empresa");
-$pdf->setY(30);$pdf->setX(10);
-$pdf->Cell(5,$textypos,"Email de la empresa"); */
+//Seteamos el inicio del margen superior en 25 pixeles
 
-// Agregamos los datos del cliente
-$pdf->SetFont('Arial','B',10);    
-$pdf->setY(10);$pdf->setX(55);
-$pdf->Cell(5,$textypos,"Usuario:");
-$pdf->SetFont('Arial','',10);    
-$pdf->setY(15);$pdf->setX(55);
-$pdf->Cell(5,$textypos,$datos);
-/* $pdf->setY(20);$pdf->setX(75);
-$pdf->Cell(5,$textypos,"Direccion del cliente");
-$pdf->setY(25);$pdf->setX(75);
-$pdf->Cell(5,$textypos,"Telefono del cliente");
-$pdf->setY(30);$pdf->setX(75);
-$pdf->Cell(5,$textypos,"Email del cliente"); */
+$y_axis_initial = 25;
 
-// Agregamos los datos del cliente
-$pdf->SetFont('Arial','B',10);    
-$pdf->setY(10);$pdf->setX(135);
+//Seteamos el tiupo de letra y creamos el titulo de la pagina. No es un encabezado no se repetira
+$pdf->SetFont('Arial','B',12);
 
-$pdf->Cell(5,$textypos,"BOLETA NRO : ".$id_pago);
-$pdf->SetFont('Arial','',10);    
-$pdf->setY(15);$pdf->setX(135);
-$pdf->Cell(5,$textypos,"Fecha : ".$fecha_pago);
-$pdf->setY(20);$pdf->setX(135);
-/* $pdf->Cell(5,$textypos,"Vencimiento: 11/ENE/2020");
-$pdf->setY(45);$pdf->setX(135); */
-$pdf->Cell(5,$textypos,"");
-$pdf->setY(30);$pdf->setX(135);
-$pdf->Cell(5,$textypos,"");
+$pdf->Cell(40,6,'',0,0,'C');
+$pdf->Cell(100,6,'LISTA DE PRODUCTOS',1,0,'C');
 
-/// Apartir de aqui empezamos con la tabla de productos
-$pdf->setY(40);$pdf->setX(135);
-    $pdf->Ln();
-/////////////////////////////
-//// Array de Cabecera
-$header = array("Nro", "Concepto","Cant.","Precio","Total");
-//// Arrar de Productos
-$products = array(
-    
-	array("01", $estado,1,$monto_usuario,0),
-	/* array("0024", "Producto 2",5,80,0),
-	array("0001", "Producto 3",1,40,0),
-	array("0001", "Producto 3",5,80,0), 
-    array("0001", "Producto 3",4,30,0),
-	array("0001", "Producto 3",7,80,0), */
-);
-    // Column widths
-    $w = array(20, 95, 20, 25, 25);
-    // Header
-    for($i=0;$i<count($header);$i++)
-        $pdf->Cell($w[$i],7,$header[$i],1,0,'C');
-    $pdf->Ln();
-    // Data
-    $total = 0;
-    foreach($products as $row)
-    {
-        $pdf->Cell($w[0],6,$row[0],1);
-        $pdf->Cell($w[1],6,$row[1],1);
-        $pdf->Cell($w[2],6,number_format($row[2]),'1',0,'R');
-        $pdf->Cell($w[3],6,"S/ ".number_format($row[3],2,".",","),'1',0,'R');
-        $pdf->Cell($w[4],6,"S/ ".number_format($row[3]*$row[2],2,".",","),'1',0,'R');
+$pdf->Ln(10);
 
-        $pdf->Ln();
-        $total+=$row[3]*$row[2];
+//Creamos las celdas para los titulo de cada columna y le asignamos un fondo gris y el tipo de letra
+$pdf->SetFillColor(232,232,232);
 
-    }
-/////////////////////////////
-//// Apartir de aqui esta la tabla con los subtotales y totales
-$yposdinamic = 40 + (count($products)*10);
+$pdf->SetFont('Arial','B',10);
+$pdf->Cell(125,6,'Titulo',1,0,'C',1);
 
-$pdf->setY($yposdinamic);
-$pdf->setX(235);
-    $pdf->Ln();
-/////////////////////////////
-$header = array("", "");
-$data2 = array(
-	array("Subtotal",$total),
-	array("Descuento", 0),
-	array("Impuesto", 0),
-	array("Total", $total),
-);
-    // Column widths
-    $w2 = array(40, 40);
-    // Header
+$pdf->Cell(30,6,'Precio',1,0,'C',1);
+$pdf->Cell(30,6,'Foto',1,0,'C',1);
 
-    $pdf->Ln();
-    // Data
-    foreach($data2 as $row)
-    {
-$pdf->setX(115);
-        $pdf->Cell($w2[0],6,$row[0],1);
-        $pdf->Cell($w2[1],6,"S/ ".number_format($row[1], 2, ".",","),'1',0,'R');
+$pdf->Ln(10);
 
-        $pdf->Ln();
-    }
-/////////////////////////////
+//Comienzo a crear las fiulas de productos según la consulta MySQL
 
-$pdf->output();
+while($fila = mysqli_fetch_array($resultado))
+{
+
+$titulo = $fila['titulo'];
+
+$precio = $fila['precio'];
+$imagen="fotos/".$row['imagen1'];
+
+$pdf->Cell(125,15,$titulo,1,0,'L',0);
+
+$pdf->Cell(30,15,$precio,1,0,'R',1);
+//Muestro la iamgen dentro de la celda GetX y GetY dan las coordenadas actuales de la fila
+
+$pdf->Cell( 30, 15, $pdf->Image($imagen, $pdf->GetX()+5, $pdf->GetY()+3, 20), 1, 0, 'C', false );
+
+$pdf->Ln(15);
+
+}
+
+mysqli_close($enlace);
+
+//Mostramos el documento pdf
+$pdf->Output();
 
 ?>
