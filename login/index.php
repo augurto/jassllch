9e2d2a49b559732f79d2d32708d2a9eada727892
Ego <augurto.17@gmail.com>
2022-08-18 01:35:16 -0500

    abora si

--- login/index.php
+++ login/index.php
@@ -52,9 +52,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $password);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
-                        if($password==$_POST["password"]){
+                        if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
                             session_start();
                             
@@ -105,7 +105,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <!-- Style -->
     <link rel="stylesheet" href="css/style.css">
 
-    <title>Jass...</title>
+    <title>Jass</title>
   </head>
   <body>
   
--- login/usuario.php
+++ login/usuario.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id_jass, nombres, dni_usuarios_jass FROM usuarios_jass WHERE dni_usuarios_jass = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -52,19 +52,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $password);
                     if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
+                        if($password==$_POST["password"]){
                             // Password is correct, so start a new session
                             session_start();
                             
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
+                            $_SESSION["id_jass"] = $id;
+                            $_SESSION["nombres"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: ../index.php");
+                            header("location: ../usuario_jass?dni=$username");
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Contrasena incorrecto.";
