8b0e6990d9f530ad42b0842f9957579de75ac88b
Ego <augurto.17@gmail.com>
2022-08-17 20:43:45 -0500

    cxvxc

--- login/index.php
+++ login/index.php
@@ -100,16 +100,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Check if username exists, if yes then verify password
             if(mysqli_stmt_num_rows($stmt) == 1){                    
                 // Bind result variables
-                mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                mysqli_stmt_bind_result($stmt, $id, $username, $password);
                 if(mysqli_stmt_fetch($stmt)){
-                    if(password_verify($password, $hashed_password)){
+                    if(password_verify($password,$password)){
                         // Password is correct, so start a new session
                         session_start();
                         
                         // Store data in session variables
                         $_SESSION["loggedin"] = true;
                         $_SESSION["id_jass"] = $id;
-                        $_SESSION["nombres"] = $username;                            
+                        $_SESSION["id_jass"] = $username;                            
                         
                         // Redirect user to welcome page
                         header("location: ../index.php");
