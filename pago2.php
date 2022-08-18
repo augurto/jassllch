8e53e6fa40f9d6f9480d09c113b6771bed1da0d8
Ego <augurto.17@gmail.com>
2022-08-18 07:58:40 -0500

    sin librerias

--- pago2.php
+++ pago2.php
@@ -26,8 +26,7 @@ $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where es
     <script src="js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
 
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
+  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css"> 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
