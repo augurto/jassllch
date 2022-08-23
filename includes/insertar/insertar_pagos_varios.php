48a8bf086fe7d833374e238c40cc60ec9ecef0a0
Ego <augurto.17@gmail.com>
2022-08-23 00:27:08 -0500

    xcvxcv

--- includes/insertar/insertar_pagos_varios.php
+++ includes/insertar/insertar_pagos_varios.php
@@ -38,20 +38,21 @@ if($dataCode >0){
             $queryInsertCode = ("INSERT INTO pagos(id_jass,dni_usuario_jass,datos_pago,pago_mes,mes_actual_pago,year_actual_pago,name_user,id_user, deuda,variable_boleta,estado_pago,pago_hoy)
              VALUES ('" .$idCliente. "','" .$dni_usuario_jass. "','" .$datos_usuario. "','" .$PRODUCTO[$i]. "','" .$mes_actual. "','" .$year_actual. "','" .$name_user. "','" .$id_user. "','" .$deuda. "','ING','0','" .$hoy. "')");
             $resultado = mysqli_query($con, $queryInsertCode);
-        }
+            echo '<p style="color:#fff;"></p>';
+              echo "<script type='text/javascript'>
+                Swal.fire(
+                'Pagos Registrados!',
+                'Operación realizada con exito',
+                'success'
+              ).then((result) => {
+                    if (result.isConfirmed) {
+                      location.href='../../usuario_jass.php?dni=$dni_usuario_jass';
+                    } 
+                  })
+              </script>";
+                      }
     } 
-echo '<p style="color:#fff;"></p>';
-echo "<script type='text/javascript'>
-  Swal.fire(
-  'Pagos Registrados!',
-  'Operación realizada con exito',
-  'success'
-).then((result) => {
-      if (result.isConfirmed) {
-        location.href='../../usuario_jass.php?dni=$dni_usuario_jass';
-      } 
-    })
-</script>";
+
 }else{
     echo 'Error';
 } 
