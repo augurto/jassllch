c62fbf55e56fd04f866de57d59e3034ea288a1fc
Ego <augurto.17@gmail.com>
2022-08-23 07:11:48 -0500

    require("../../config/config.php"); require_once ('../../config/conexion_tabla.php');

--- pagos_varios.php
+++ pagos_varios.php
@@ -11,6 +11,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
+
+require_once ('config/conexion_tabla.php');
 $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where estado='terminado'");
         $rwt=mysqli_fetch_array($sald);
         $saldo=$rwt['saldo'];
