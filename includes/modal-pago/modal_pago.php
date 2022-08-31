8bad3d21cd10489b425d0da004ae243740d7565c
Ego <augurto@gmail.com>
2022-08-31 02:15:43 -0500

    monstrando anio en pago

--- includes/modal-pago/modal_pago.php
+++ includes/modal-pago/modal_pago.php
@@ -135,7 +135,7 @@
                         
                         <input type="hidden" class="form-control" id="mes_actual" name="mes_actual" aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $mes_actual;?>" readonly >
                        <!--  <span class="input-group-text" id="basic-addon1">Año</span> -->
-                        <input type="hidden" class="form-control" id="year_actual" name="year_actual"  aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $year_actual;?>"  readonly>
+                        <input type="number" class="form-control" id="year_actual" name="year_actual"  aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $year_actual;?>"  >
                         
                         </div>
                         <div class="input-group mb-3">
