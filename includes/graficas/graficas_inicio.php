ea2f3b8897dd7749539eaca1db3797e54f978742
Ego <augurto@gmail.com>
2022-09-01 04:55:38 -0500

    remeemplazando reporte general

--- includes/graficas/graficas_inicio.php
+++ includes/graficas/graficas_inicio.php
@@ -68,12 +68,12 @@
             
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
-              <a href="../../reporte_general.php">
+              <a href="../../otros_pagos.php">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Reporte General </div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tes;?></div>
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Otros Pagos </div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><!-- <?php echo $tes;?> --></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
