2fc68e13b49713579e5eca42f3078515976dd96f
Ego <augurto@gmail.com>
2022-09-03 03:43:43 -0500

    mes fin

--- includes/process/nuevo_pago.php
+++ includes/process/nuevo_pago.php
@@ -12,6 +12,7 @@ $dni=$_GET["dnijass"];
 $datos=$_GET["datos"];
 $hoy=$_GET["hoy"];
 $mes_actual=$_GET["mes_actual"];
+$cantidadmes=$_GET["cantidadmes"];
 $year_actual=$_GET["year_actual"];
 $name_user=$_GET["name_user"];
 $id_user=$_GET["id_user"];
@@ -19,7 +20,7 @@ $monto_pago=$_GET["monto_pago"];
 $deuda=$_GET["deuda"];
 $vuelto=$_GET["vuelto"];
 $mes_pago=$_GET["mes_pago"];
-
+$mes_fin=$mes_actual+$cantidadmes-1;
 date_default_timezone_set("America/Lima");
 $hoy2 = date("Y-m-d H:i:s");
 
@@ -29,9 +30,9 @@ if (!$conn) {
  
 echo "Connected successfully";
  
-$sql = "INSERT INTO pagos (id_jass, dni_usuario_jass, datos_pago, pago_hoy, mes_actual_pago,year_actual_pago,name_user,id_user, monto_pago, deuda, vuelto, pago_mes,variable_boleta, estado_pago) 
+$sql = "INSERT INTO pagos (id_jass, dni_usuario_jass, datos_pago, pago_hoy, mes_actual_pago,year_actual_pago,name_user,id_user, monto_pago, deuda, vuelto, pago_mes,variable_boleta, estado_pago,mes_inicio, mes_fin) 
 
-VALUES ('$jass','$dni', '$datos', '$hoy2','$mes_actual', '$year_actual', '$name_user', '$id_user', '$monto_pago', '$deuda', '$vuelto', '$mes_pago','ING', '0')";
+VALUES ('$jass','$dni', '$datos', '$hoy2','$mes_actual', '$year_actual', '$name_user', '$id_user', '$monto_pago', '$deuda', '$vuelto', '$mes_pago','ING', '0', '$mes_actual', '$mes_fin')";
 if (mysqli_query($conn, $sql)) {
     echo"<script language ='JavaScript'>";
       echo "location='../../../usuario_jass.php?dni=$dni'";
