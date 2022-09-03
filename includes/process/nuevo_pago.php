cf08d6126e00c27288b25dcd302ed2c90f19da88
Ego <augurto@gmail.com>
2022-09-03 04:20:31 -0500

    guardando nombre de mes

--- includes/process/nuevo_pago.php
+++ includes/process/nuevo_pago.php
@@ -7,6 +7,7 @@ $password = "JassJass*#17";
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 
+
 $jass=$_GET["idjass"];
 $dni=$_GET["dnijass"];
 $datos=$_GET["datos"];
@@ -24,6 +25,11 @@ $mes_fin=$mes_actual+$cantidadmes-1;
 date_default_timezone_set("America/Lima");
 $hoy2 = date("Y-m-d H:i:s");
 
+setlocale(LC_ALL, 'spanish');
+$monthNum  = 2;
+$dateObj   = DateTime::createFromFormat('!m', $mes_fin);
+$monthName = strftime('%B', $dateObj->getTimestamp());
+$mayus_mes=ucfirst($monthName);
 if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
 }
@@ -32,7 +38,7 @@ echo "Connected successfully";
  
 $sql = "INSERT INTO pagos (id_jass, dni_usuario_jass, datos_pago, pago_hoy, mes_actual_pago,year_actual_pago,name_user,id_user, monto_pago, deuda, vuelto, pago_mes,variable_boleta, estado_pago,mes_inicio, mes_fin,concepto_pago,cantidad_mes) 
 
-VALUES ('$jass','$dni', '$datos', '$hoy2','$mes_actual', '$year_actual', '$name_user', '$id_user', '$monto_pago', '$deuda', '$vuelto', '$mes_pago','ING', '0', '$mes_actual', '$mes_fin', 'Pago de Agua mes :', '$cantidadmes')";
+VALUES ('$jass','$dni', '$datos', '$hoy2','$mes_actual', '$year_actual', '$name_user', '$id_user', '$monto_pago', '$deuda', '$vuelto', '$mes_pago','ING', '0', '$mes_pago', '$mayus_mes', 'Pago de Agua mes :', '$cantidadmes')";
 if (mysqli_query($conn, $sql)) {
     echo"<script language ='JavaScript'>";
       echo "location='../../../usuario_jass.php?dni=$dni'";
