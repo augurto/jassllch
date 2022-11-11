828a13343a721dee386f8fdb78577d3d3d9e5892
Ego <augurto.17@gmail.com>
2022-11-11 00:34:08 -0500

    sin monto decimal

--- includes/graficas/graficas_inicio.php
+++ includes/graficas/graficas_inicio.php
@@ -30,7 +30,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Saldo</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">S/<?php echo number_format($total_monto,2).'-'.$monto_decimal;?></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">S/<?php echo number_format($total_monto,2);?></div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-dollar-sign  fa-2x text-gray-300"></i>
