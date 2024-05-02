318098e42136200ac45a9039df91b35a00b47d2f
Ego <augurto.17@gmail.com>
2024-05-02 14:33:23 -0500

    cambio de bd

--- config/conexion_tabla.php
+++ config/conexion_tabla.php
@@ -1,5 +1,5 @@
 <?php
         // Te recomiendo utilizar esta conección, la que utilizas ya no es la recomendada. 
-        $link = new PDO('mysql:host=localhost;dbname=u291982824_agua', 'u291982824_agua', 'JassJass*#17'); // el campo vaciío es para la password. 
+        $link = new PDO('mysql:host=localhost;dbname=u291982824_agua', 'u291982824_agua', 'JassJass*#17'); // el campo vaciío es para la password.  
 
 ?>
