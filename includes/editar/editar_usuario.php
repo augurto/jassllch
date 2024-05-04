44c43e410ad8f276cf4ef79308633b487f81383e
Ego <augurto.17@gmail.com>
2024-05-03 23:45:07 -0500

    mostrar editar todo

--- includes/editar/editar_usuario.php
+++ includes/editar/editar_usuario.php
@@ -1,84 +1,125 @@
+<!-- <script>
+    $(document).on("click", "#btnmodal0", function() {
 
-<script >
-                                      $(document).on("click", "#btnmodal0",function () {
-                                        
-                                              var idjass2 =$(this).data('idjass2');
-                                              var nombres =$(this).data('nombres');
-                                              var paterno =$(this).data('ape');
-                                              var materno =$(this).data('ape_mat');
-                                              var estado_civil =$(this).data('estado_civil');
-                                              var jass_dni =$(this).data('jass_dni');
-                                             
+        var idjass2 = $(this).data('idjass2');
+        var nombres = $(this).data('nombres');
+        var paterno = $(this).data('ape');
+        var materno = $(this).data('ape_mat');
+        var estado_civil = $(this).data('estado_civil');
+        var jass_dni = $(this).data('jass_dni');
 
-                                        $("#idjass2").val(idjass2);
-                                        $("#nombres").val(nombres);
 
-                                        
-                                        $("#paterno").val(paterno);
-                                        $("#materno").val(materno);
-                                        $("#datos").val(nombres+" "+paterno+" "+materno);
-                                        $("#estado_civil").val(estado_civil);
-                                        $("#jass_dni").val(jass_dni);
-                          
-                                      })
+        $("#idjass2").val(idjass2);
+        $("#nombres").val(nombres);
 
-                                    </script>
+
+        $("#paterno").val(paterno);
+        $("#materno").val(materno);
+        $("#datos").val(nombres + " " + paterno + " " + materno);
+        $("#estado_civil").val(estado_civil);
+        $("#jass_dni").val(jass_dni);
+
+    })
+</script> -->
+
+<script>
+    $(document).on("click", "#btnmodal0", function() {
+        var idjass2 = $(this).data('idjass2');
+        var nombres = $(this).data('nombres');
+        var paterno = $(this).data('ape');
+        var materno = $(this).data('ape_mat');
+        var estado_civil = $(this).data('estado_civil');
+        var jass_dni = $(this).data('jass_dni');
+        var sector = $(this).data('sector');
+        var cantidad = $(this).data('cantidad');
+        var natural = $(this).data('natural');
+
+        // Asignar valores a los campos del formulario en el modal
+        $("#idjass2").val(idjass2);
+        $("#nombres").val(nombres);
+        $("#paterno").val(paterno);
+        $("#materno").val(materno);
+        $("#estado_civil").val(estado_civil);
+        $("#jass_dni").val(jass_dni);
+        $("#sector").val(sector);
+        $("#cantidad_miembros").val(cantidad);
+        $("#natural_de").val(natural);
+
+        // Opcional: Puedes mostrar los datos en un campo adicional
+        $("#datos").val(nombres + " " + paterno + " " + materno);
+    });
+</script>
+
+
 
 <!-- Modal -->
-    <div class="modal fade" id="ModalEdit0" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Editar Usuario</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                    </div>
-                    
-                    <label for=""><?php
-                        $var_PHP = "<script> document.write(estado) </script>"; // igualar el valor de la variable JavaScript a PHP 
+<div class="modal fade" id="ModalEdit0" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Editar Usuario</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
 
-                    echo $var_PHP   // muestra el resultado 
+            <label for=""><?php
+                            $var_PHP = "<script> document.write(estado) </script>"; // igualar el valor de la variable JavaScript a PHP 
 
-                    ?></label>
-              
+                            echo $var_PHP   // muestra el resultado 
 
-                    <form action="../../includes/process/update_usuario.php" name="calculadora">
-                        <div class="modal-body">
-                        <div class="input-group mb-3">
-                       <!--  <span class="input-group-text" id="basic-addon1">Cod Usuario</span> -->
+                            ?></label>
+
+
+            <form action="../../includes/process/update_usuario.php" name="calculadora">
+                <div class="modal-body">
+                    <div class="input-group mb-3">
+                        <!--  <span class="input-group-text" id="basic-addon1">Cod Usuario</span> -->
                         <input type="hidden" class="form-control" id="idjass2" name="idjass2" aria-label="Username" aria-describedby="basic-addon1" readonly>
-                        </div>
-                        <div class="input-group mb-3">
+                    </div>
+                    <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1">DNI</span>
-                        
-                        <input type="text" class="form-control" id="jass_dni" name="jass_dni" aria-label="DNI" aria-describedby="basic-addon1"  readonly>
-                        </div>
-                     
-                        <?php 
-                        $mes_actual =date("m");
-                        $year_actual =date("Y");
-                        date_default_timezone_set("America/Lima");
-                        $hoy = date("Y-m-d H:i:s");     
-                        ?>
-                      
-                      <div class="input-group mb-3">
-                                <label class="input-group-text" for="inputGroupSelect01">Nombres</label>
-                                <input type="text" class="form-control" id="nombres" autofocus name="nombres" aria-label="nombres" aria-describedby="basic-addon1"   required >
-                                        
-                        </div> 
-                        <div class="input-group mb-3">
-                                <label class="input-group-text" for="inputGroupSelect01">Apellido Paterno</label>
-                                <input type="text" class="form-control" id="paterno" autofocus name="paterno" aria-label="DNI" aria-describedby="basic-addon1"   required >
-                                        
-                        </div>   
-                        <div class="input-group mb-3">
-                                
-                                <label class="input-group-text" for="inputGroupSelect01">Apellido Materno</label>
-                                <input type="text" class="form-control" id="materno" name="materno" aria-label="DNI" aria-describedby="basic-addon1"  >
-                                        
-                        </div>  
-                        <!-- <div class="input-group mb-3">
+
+                        <input type="text" class="form-control" id="jass_dni" name="jass_dni" aria-label="DNI" aria-describedby="basic-addon1" readonly>
+                    </div>
+
+                    <?php
+                    $mes_actual = date("m");
+                    $year_actual = date("Y");
+                    date_default_timezone_set("America/Lima");
+                    $hoy = date("Y-m-d H:i:s");
+                    ?>
+
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupSelect01">Nombres</label>
+                        <input type="text" class="form-control" id="nombres" autofocus name="nombres" aria-label="nombres" aria-describedby="basic-addon1" required>
+
+                    </div>
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupSelect01">Apellido Paterno</label>
+                        <input type="text" class="form-control" id="paterno" autofocus name="paterno" aria-label="DNI" aria-describedby="basic-addon1" required>
+
+                    </div>
+                    <div class="input-group mb-3">
+
+                        <label class="input-group-text" for="inputGroupSelect01">Apellido Materno</label>
+                        <input type="text" class="form-control" id="materno" name="materno" aria-label="DNI" aria-describedby="basic-addon1">
+
+                    </div>
+                    <!-- Agregar más campos aquí -->
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Sector</span>
+                        <input type="text" class="form-control" id="sector" name="sector">
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Cantidad de Miembros</span>
+                        <input type="text" class="form-control" id="cantidad_miembros" name="cantidad_miembros">
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">Natural de</span>
+                        <input type="text" class="form-control" id="natural_de" name="natural_de">
+                    </div>
+                    <!-- <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1">Estado Civil</span>
                         
                         <select class="form-select" aria-label="Default select example" name="estado_civil" id="estado_civil">
@@ -91,40 +132,39 @@
                             <option value="Otro">Otro</option>
                         </select>
                         </div>   -->
-                        <div class="input-group mb-3">
-                       <!--  <span class="input-group-text" id="basic-addon1">Hoy</span> -->
-                        <input type="hidden" class="form-control" id="hoy" name="hoy"  aria-label="hoy" aria-describedby="basic-addon1" value="<?php echo $hoy;?>"  readonly >
-                                               
-                        </div>
-                        <div class="input-group mb-3">
-                        
-                        
-                       <!--  <span class="input-group-text" id="basic-addon1">Mes</span> -->
-                        
-                        <input type="hidden" class="form-control" id="mes_actual" name="mes_actual" aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $mes_actual;?>" readonly >
-                       <!--  <span class="input-group-text" id="basic-addon1">Año</span> -->
-                        <input type="hidden" class="form-control" id="year_actual" name="year_actual"  aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $year_actual;?>"  readonly>
-                        
-                        </div>
-                        <div class="input-group mb-3">
-                      <!--   <span class="input-group-text" id="basic-addon1">Usuario</span> -->
-                        <input type="hidden" class="form-control" id="name_user" name="name_user"  aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $usuario;?>"  readonly>
-                        <input type="hidden" class="form-control" id="id_user" name="id_user" aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $id_usuario;?>"  readonly>
-                       
-                       
-                        </div>
-                                    
-                        
+                    <div class="input-group mb-3">
+                        <!--  <span class="input-group-text" id="basic-addon1">Hoy</span> -->
+                        <input type="hidden" class="form-control" id="hoy" name="hoy" aria-label="hoy" aria-describedby="basic-addon1" value="<?php echo $hoy; ?>" readonly>
 
-                        
-                                          
-                        </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                        <button type="submit" class="btn btn-primary">Actualizar</button>
                     </div>
-                    </form>
-            </div>
+                    <div class="input-group mb-3">
+
+
+                        <!--  <span class="input-group-text" id="basic-addon1">Mes</span> -->
+
+                        <input type="hidden" class="form-control" id="mes_actual" name="mes_actual" aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $mes_actual; ?>" readonly>
+                        <!--  <span class="input-group-text" id="basic-addon1">Año</span> -->
+                        <input type="hidden" class="form-control" id="year_actual" name="year_actual" aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $year_actual; ?>" readonly>
+
+                    </div>
+                    <div class="input-group mb-3">
+                        <!--   <span class="input-group-text" id="basic-addon1">Usuario</span> -->
+                        <input type="hidden" class="form-control" id="name_user" name="name_user" aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $usuario; ?>" readonly>
+                        <input type="hidden" class="form-control" id="id_user" name="id_user" aria-label="DNI" aria-describedby="basic-addon1" value="<?php echo $id_usuario; ?>" readonly>
+
+
+                    </div>
+
+
+
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Actualizar</button>
+                </div>
+            </form>
         </div>
     </div>
-
+</div>
--- includes/usuarios_jass.php
+++ includes/usuarios_jass.php
@@ -1,56 +1,56 @@
+<!--Ejemplo tabla con DataTables-->
+<?php require_once('./config/conexion_tabla.php') ?>
+<div class="container-fluid ">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="table-responsive">
+                <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
 
-        <!--Ejemplo tabla con DataTables-->
-        <?php require_once ('./config/conexion_tabla.php') ?>
-        <div class="container-fluid ">
-        <div class="row">
-                <div class="col-lg-12">
-                    <div class="table-responsive">        
-                        <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                        
-                        <thead>
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Usuario</th>
-                                    <th>A. Paterno</th>
-                                    <th>A. Materno</th>
-                                    <th>DNI</th>
-                                    <th>Sector</th>
-                                    <th>Cant. Fam.</th>
-                                    <th>Natural de</th>
-                                   <!--  <th>Estado</th> -->
-                                    <th>Accion</th>
-                                    
-                                </tr>
-                                </thead>
-                             
-                        <?php 
-                           $count=1;
-                        foreach ($link->query('SELECT * from usuarios_jass where estado_usuario_jass = 0') as $row){ // aca se hace la consulta e iterarla con each. ?> 
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Usuario</th>
+                            <th>A. Paterno</th>
+                            <th>A. Materno</th>
+                            <th>DNI</th>
+                            <th>Sector</th>
+                            <th>Cant. Fam.</th>
+                            <th>Natural de</th>
+                            <!--  <th>Estado</th> -->
+                            <th>Accion</th>
+
+                        </tr>
+                    </thead>
+
+                    <?php
+                    $count = 1;
+                    foreach ($link->query('SELECT * from usuarios_jass where estado_usuario_jass = 0') as $row) { // aca se hace la consulta e iterarla con each. 
+                    ?>
                         <?php
-                        $id_jass=$row['id_jass'];
-                        $dni_jass=$row['dni_usuario_jass'];
-                        $nombre=$row['nombres'];
-                        $ap_paterno=$row['ap_paterno'];
-                        $ap_materno=$row['ap_materno'];
-                        $estado_civil=$row['estado_civil'];
-                        $estado=$row['estado'];
-                        $sector=$row['sector_jass'];
-                        $natural_de=$row['natural_de'];
-                        $cantidad_miembros=$row['cantidad_miembros'];
-                         if ($estado==0) { ?>
-                             
-                             <tr style="background-color: #F0FFFF !important;">
+                        $id_jass = $row['id_jass'];
+                        $dni_jass = $row['dni_usuario_jass'];
+                        $nombre = $row['nombres'];
+                        $ap_paterno = $row['ap_paterno'];
+                        $ap_materno = $row['ap_materno'];
+                        $estado_civil = $row['estado_civil'];
+                        $estado = $row['estado'];
+                        $sector = $row['sector_jass'];
+                        $natural_de = $row['natural_de'];
+                        $cantidad_miembros = $row['cantidad_miembros'];
+                        if ($estado == 0) { ?>
 
-                             <?php  }elseif ($estado==1) { ?>
-                             <tr style="background-color: #F0FFF0 !important;">
+                            <tr style="background-color: #F0FFFF !important;">
 
-                             <?php  }elseif ($estado==2) { ?>
-                             <tr style="background-color: #FFE4E1 !important;">
-                       
-                             <?php } else{?>
-                             <tr style="background-color: #FDF5E6 !important;">
-                             <?php }?>   
-                            
+                            <?php  } elseif ($estado == 1) { ?>
+                            <tr style="background-color: #F0FFF0 !important;">
+
+                            <?php  } elseif ($estado == 2) { ?>
+                            <tr style="background-color: #FFE4E1 !important;">
+
+                            <?php } else { ?>
+                            <tr style="background-color: #FDF5E6 !important;">
+                            <?php } ?>
+
                             <td><?php echo $count++; ?></td>
                             <td><a href="../../usuario_jass.php?dni=<?php echo $dni_jass; ?>"><?php echo $nombre; ?></a></td>
                             <td><?php echo $ap_paterno ?></td>
@@ -59,41 +59,46 @@
                             <td><?php echo $sector ?></td>
                             <td><?php echo $cantidad_miembros ?></td>
                             <td><?php echo $natural_de ?></td>
-                          <!--   <td><?php if ($estado==0) {
-                             
-                                echo 'Activo';
-                            } elseif ($estado==1) {
-                               
-                                echo 'Terminado';
-                            } elseif ($estado==2) {
-                               
-                                echo 'Retirado';
-                            }?></td> -->
-                            <td>
-                                         <button type="button" id="btnmodal0" class="btn btn-primary" data-toggle="modal" data-target="#ModalEdit0" data-idjass2="<?php echo $id_jass; ?>" data-nombres="<?php echo $nombre; ?>" data-ape="<?php echo $ap_paterno;?>" data-ape_mat="<?php echo $ap_materno;?>" data-estado_civil="<?php echo $estado_civil;  ?>" data-jass_dni="<?php echo $dni_jass;  ?>" >
-                                        <i class="fa fa-edit"></i>
-                                        </button>
-                         
-                                        <button type="button" id="btnmodal" class="btn btn-dark" data-toggle="modal" data-target="#ModalEdit" data-idjass="<?php echo $id_jass; ?>" data-nombre="<?php echo $nombre; ?>" data-paterno="<?php echo $ap_paterno;?>" data-materno="<?php echo $ap_materno;  ?>" data-dnijass="<?php echo $dni_jass;  ?>" >
-                                         <i class="fa fa-plus"></i>
-                                         
-                                        </button>
+                            <!--   <td><?php if ($estado == 0) {
 
-                                        <button type="button" id="btnmodall" class="btn btn-warning" data-toggle="modal" data-target="#ModalEditt" data-unno="<?php echo $id_jass; ?>" data-dos="<?php echo $nombre; ?>" data-tres="<?php echo $ap_paterno;?>" data-cuatro="<?php echo $ap_materno;  ?>" data-dnijass3="<?php echo $dni_jass;  ?>" >
-                                         <i class="fa fa-plus"></i>
-                                         
-                                        </button>
-                                        
-                                        <a href="includes/insertar/actualizar_eliminar.php?id=<?php echo $id_jass; ?>"><i class="fa fa-trash"></i></a>
-                                        
+                                            echo 'Activo';
+                                        } elseif ($estado == 1) {
+
+                                            echo 'Terminado';
+                                        } elseif ($estado == 2) {
+
+                                            echo 'Retirado';
+                                        } ?></td> -->
+                            <td>
+                                <!-- <button type="button" id="btnmodal0" class="btn btn-primary" data-toggle="modal" data-target="#ModalEdit0" data-idjass2="<?php echo $id_jass; ?>" data-nombres="<?php echo $nombre; ?>" data-ape="<?php echo $ap_paterno; ?>" data-ape_mat="<?php echo $ap_materno; ?>" data-estado_civil="<?php echo $estado_civil;  ?>" data-jass_dni="<?php echo $dni_jass;  ?>" >
+                                        <i class="fa fa-edit"></i>
+                                        </button> -->
+
+                                <button type="button" id="btnmodal0" class="btn btn-primary" data-toggle="modal" data-target="#ModalEdit0" data-idjass2="<?php echo $id_jass; ?>" data-nombres="<?php echo $nombre; ?>" data-ape="<?php echo $ap_paterno; ?>" data-ape_mat="<?php echo $ap_materno; ?>" data-estado_civil="<?php echo $estado_civil; ?>" data-jass_dni="<?php echo $dni_jass; ?>" data-sector="<?php echo $sector; ?>" data-cantidad="<?php echo $cantidad_miembros; ?>" data-natural="<?php echo $natural_de; ?>">
+                                    <i class="fa fa-edit"></i> Editar
+                                </button>
+
+
+                                <button type="button" id="btnmodal" class="btn btn-dark" data-toggle="modal" data-target="#ModalEdit" data-idjass="<?php echo $id_jass; ?>" data-nombre="<?php echo $nombre; ?>" data-paterno="<?php echo $ap_paterno; ?>" data-materno="<?php echo $ap_materno;  ?>" data-dnijass="<?php echo $dni_jass;  ?>">
+                                    <i class="fa fa-plus"></i>
+
+                                </button>
+
+                                <button type="button" id="btnmodall" class="btn btn-warning" data-toggle="modal" data-target="#ModalEditt" data-unno="<?php echo $id_jass; ?>" data-dos="<?php echo $nombre; ?>" data-tres="<?php echo $ap_paterno; ?>" data-cuatro="<?php echo $ap_materno;  ?>" data-dnijass3="<?php echo $dni_jass;  ?>">
+                                    <i class="fa fa-plus"></i>
+
+                                </button>
+
+                                <a href="includes/insertar/actualizar_eliminar.php?id=<?php echo $id_jass; ?>"><i class="fa fa-trash"></i></a>
+
                             </td>
-                        </tr>
+                            </tr>
                         <?php
-                            }
+                    }
                         ?>
-                        </table>
-                </table>          
-                    </div>
-                </div>
-        </div>  
-    </div>    
+                </table>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
