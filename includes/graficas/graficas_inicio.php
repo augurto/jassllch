3e5328073600075a3e7af7073780f9e6d23600c3
Ego <augurto.17@gmail.com>
2025-02-18 03:01:20 -0500

    XD

--- includes/graficas/graficas_inicio.php
+++ includes/graficas/graficas_inicio.php
@@ -18,7 +18,7 @@
       $monto_decimal=number_format($soles,2) - number_format($tin,2);
       $total_monto=$soles-$tin;
 /* 		$sql="SELECT * FROM  proyecto order by id desc"; */
-		$query = mysqli_query($con, $sql);         
+	/* 	$query = mysqli_query($con, $sql);       */   
 			?>
 <div class="container">
 <div class="row">
