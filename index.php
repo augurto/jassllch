6f54d46c45bf953dd1d5cac762915ca8812d8236
Ego <augurto.17@gmail.com>
2025-02-18 02:57:32 -0500

    comentando proyecto

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
 require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
-$sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where estado='terminado'");
+/* $sald=mysqli_query($con,"SELECT Sum(presupuesto) as saldo FROM proyecto where estado='terminado'");
         $rwt=mysqli_fetch_array($sald);
         $saldo=$rwt['saldo'];
         $usuario=$_SESSION["username"];
-        $id_usuario=$_SESSION["id"];
+        $id_usuario=$_SESSION["id"]; */
         
 ?>
 <!DOCTYPE html>
